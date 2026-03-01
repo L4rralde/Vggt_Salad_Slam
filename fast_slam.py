278845844fef49a9a1ed058c61165eb98144a886
l4rralde <ealarralde@gmail.com>
2026-03-01 13:25:16 -0600

    Adding windows to visualize video and keyframes, fix aligning process and small changes

--- fast_slam.py
+++ fast_slam.py
@@ -1,3 +1,4 @@
+import os
 from typing import List, Tuple, Any
 from multiprocessing import Queue
 from dataclasses import dataclass
@@ -24,6 +25,9 @@ def cv2_to_pil(img: np.ndarray) -> Image.Image:
     pil_frame = Image.fromarray(cv_frame_rgb)
     return pil_frame
 
+def pil_to_cv2(img: Image.Image) -> np.ndarray:
+    pil_data = img.convert('RGB')
+    return np.array(pil_data)[:, :, ::-1]
 
 def video_publisher(video_path: str, frame_q: Queue) -> None:
     video = cv2.VideoCapture(video_path)
@@ -42,10 +46,15 @@ def video_publisher(video_path: str, frame_q: Queue) -> None:
         pil_img = cv2_to_pil(frame)
         stamp = frame_cnt/fps
         id = frame_cnt + 1
+        if frame_q.full():
+            print("[WARNING], frame_q is full.")
         frame_q.put(Frame(id, stamp, pil_img))
         frame_cnt += 1
+        cv2.imshow('video', frame)
+        cv2.waitKey(25)
 
     video.release()
+    cv2.destroyWindow("video")
 
 
 class KeyFramesDetector:
@@ -57,7 +66,7 @@ class KeyFramesDetector:
         self,
         frame_ids: List[int],
         view_preds: Dict[str, torch.Tensor],
-        th: float=0.6
+        th: float=0.7
     ) -> Tuple[List[int], Dict[str, torch.Tensor]]:
         descriptors = view_preds.descriptor
 
@@ -89,6 +98,16 @@ class KeyFramesDetector:
         return kf_ids, kf_preds
 
 
+def keyframe_publisher(keyframe_q: Queue) -> None:
+    while True:
+        kf: Frame|None = keyframe_q.get()
+        if kf is None:
+            break
+        kf = pil_to_cv2(kf.img)
+        cv2.imshow("keyframes", kf)
+        cv2.waitKey(25)
+    cv2.destroyWindow("keyframes")
+
 @dataclass
 class ViewToken:
     frame_id: int
@@ -96,13 +115,17 @@ class ViewToken:
     processed_img: torch.Tensor
 
 
-def video_processing(frame_q: Queue, preds_q: Queue) -> None:
+def video_processing(frame_q: Queue, kframes_q: Queue, preds_q: Queue) -> None:
     from src.models import get_model
 
+    print("Loading model...")
+    start = perf_counter()
     model = get_model(
         'da3-small',
         '/home/emmanuel/Desktop/tesis/Visual_Place_Recognition'
     )
+    end = perf_counter()
+    print(f'Finished loading model. Took {end - start:.2f} seconds')
 
     from src.storage import(
         FrameRepository,
@@ -113,11 +136,11 @@ def video_processing(frame_q: Queue, preds_q: Queue) -> None:
 
     frames = FrameRepository('output/frames') #Frames and metadata in disk
     keyframes_cp = SoftLink('output/keyframes', frames.root)
-    frames_cache = FIFOCache(60) #Last 60 frames in ram.
+    frames_cache = FIFOCache(128)
     descriptors = TensorRepository('output/viewpreds/descriptors')
     patch_tokens = TensorRepository('output/viewpreds/patch_tokens')
     processed_imgs = TensorRepository('output/viewpreds/processed_imgs')
-    viewpreds_cache = FIFOCache(50)
+    viewpreds_cache = FIFOCache(64)
 
     to_encode_ids = [] #Ids to encode.
     to_predict_ids = []
@@ -129,15 +152,15 @@ def video_processing(frame_q: Queue, preds_q: Queue) -> None:
         frame: Frame = frame_q.get()
         if frame is None:
             preds_q.put(None)
+            kframes_q.put(None)
             break
-        #print(f"Received frame with id: {frame.id}")
         _id = frames.append(frame.img, frame.stamp) #Save frame in disk
         if _id != frame.id:
             raise RuntimeError(f"Packet loss: ({frame.id}, {_id})")
         frames_cache.append(frame.id, frame) #Add frame to cache
         to_encode_ids.append(frame.id)
 
-        if len(to_encode_ids) < 10: #If the number of frames not encoded is not 10, receive another frame
+        if len(to_encode_ids) < 32: #If the number of frames not encoded is not 10, receive another frame
             continue
 
         #2. Encode the batch
@@ -146,10 +169,8 @@ def video_processing(frame_q: Queue, preds_q: Queue) -> None:
             for id in to_encode_ids
         ]
         start = perf_counter()
-        #print(f"Encoding {len(to_encode_ids)} images with ids: {to_encode_ids}")
         view_preds = model.views_encoding(image_batch)
         end = perf_counter()
-        #print(f"Took {end - start:.3f} seconds")
         encoded_ids = to_encode_ids
         to_encode_ids = []
 
@@ -157,11 +178,14 @@ def video_processing(frame_q: Queue, preds_q: Queue) -> None:
         kf_ids, kf_preds = keyframe_detector(encoded_ids, view_preds)
         if not kf_ids:
             continue
+
         print(f"Found new keyframes: {kf_ids}")
-        #Saving keyframes
+        #Publish and save keyframes:
         for id in kf_ids:
+            #kf = frames_cache.get(id)
+            #kframes_q.put(kf) #Publish
             path = frames.get_path(id)
-            keyframes_cp.copy(path)
+            keyframes_cp.copy(path) #Save
 
         #Saving filtered predictions in disk and cache
         zip_iter = zip(kf_ids, kf_preds.patch_tokens, kf_preds.images, kf_preds.descriptor)
@@ -176,93 +200,86 @@ def video_processing(frame_q: Queue, preds_q: Queue) -> None:
         gc.collect()
 
         to_predict_ids += kf_ids
-        if len(to_predict_ids) < 10:
+        if len(to_predict_ids) < 4:
             continue
         #3 Chunk Sequence prediction
         #Preparing chunk
-        chunk_ids = predicted_ids[-10:] + to_predict_ids #Ids of frames to chunk
+        chunk_ids = predicted_ids[-4:] + to_predict_ids #Ids of frames to chunk
         chunk_tokens: List[ViewToken] = [viewpreds_cache.get(id) for id in chunk_ids]
         chunk_preds = Dict()
         chunk_preds.patch_tokens = torch.stack([token.token for token in chunk_tokens])
         chunk_preds.images = torch.stack([token.processed_img for token in chunk_tokens])
-        print(f"Processing sequence of size {len(chunk_ids)} with ids: {predicted_ids[-10:]}, {to_predict_ids}")
+        print(f"Processing sequence of size {len(chunk_ids)}...")
         start = perf_counter()
         preds = model.chunk_prediction(chunk_preds)
         end = perf_counter()
-        print(f"Took {end - start:.3f} seconds")
+        print(f"Sequence prediction took {end - start:.3f} seconds")
         predicted_ids += to_predict_ids
         to_predict_ids = []
 
         preds['ids'] = np.asarray(chunk_ids, dtype=np.uint32)
+        if preds_q.full():
+            print("[WARNING] preds_q is full")
         preds_q.put(preds)
 
 
-def prediction_alignment(predictions_q: Queue) -> None:
-    import os
-    from src.sim3 import VggtlongAlign, unproject_depth_map_to_point_map
+def prediction_aligning(predictions_q: Queue) -> None:
+    from src.sim3 import VggtlongAlign
     registered_ids = []
     prev_preds: Dict[str, np.ndarray] = {}
     chunk_cnt = 0
+    os.makedirs('preds', exist_ok=True)
     while True:
         curr_preds = predictions_q.get()
         if curr_preds is None:
             break
 
-        if not prev_preds:
-            prev_preds = curr_preds
-            registered_ids += list(curr_preds.ids)
-            continue
+        if prev_preds:
+            start = perf_counter()
+            print(f"Aligning scenes...")
+            curr_preds = VggtlongAlign().fit_transform(prev_preds, curr_preds)
+            end = perf_counter()
+            print(f"Aligning took {end - start:.4f} seconds")
 
-        curr_preds.world_points = unproject_depth_map_to_point_map(
-            curr_preds.depth,
-            curr_preds.intrinsics,
-            curr_preds.extrinsics
-        )
-        path = f'./preds/{chunk_cnt}.npz'
-        os.makedirs('preds', exist_ok=True)
-        np.savez(path, **curr_preds)
-
-        continue
-        start = perf_counter()
-        print(f"Aligning scenes with ids {prev_preds.ids}, {curr_preds.ids}")
-        curr_preds = VggtlongAlign().fit_transform(prev_preds, curr_preds)
-        curr_preds.world_points = unproject_depth_map_to_point_map(
-            curr_preds.depth,
-            curr_preds.intrinsics,
-            curr_preds.extrinsics
-        )
-        end = perf_counter()
-        print(f"Took {end - start:.4f} seconds")
         prev_preds = curr_preds
         registered_ids += list(curr_preds.ids)
+        path = f'./preds/{chunk_cnt}.npz'
+        np.savez(path, **curr_preds)
 
         chunk_cnt += 1
 
 
 def main():
     mp.set_start_method("spawn")
-    q_frames = mp.Queue(maxsize=10)
+    q_frames = mp.Queue(256)
     q_preds = mp.Queue(maxsize=8)
+    q_kframes = mp.Queue(maxsize=8)
     p_publisher = mp.Process(
         target=video_publisher,
         args=("/home/emmanuel/Downloads/cimat_loop.mp4", q_frames)
     )
     p_processing = mp.Process(
         target=video_processing,
-        args=(q_frames, q_preds)
+        args=(q_frames, q_kframes, q_preds)
     )
-    p_alignment = mp.Process(
-        target=prediction_alignment,
+    p_aligning = mp.Process(
+        target=prediction_aligning,
         args=(q_preds, )
     )
+    #p_kfpublisher = mp.Process(
+    #    target=keyframe_publisher,
+    #    args=(q_kframes, )
+    #)
 
     p_publisher.start()
     p_processing.start()
-    p_alignment.start()
+    p_aligning.start()
+    #p_kfpublisher.start()
 
     p_publisher.join()
     p_processing.join()
-    p_alignment.join()
+    p_aligning.join()
+    #p_kfpublisher.join()
 
 
 if __name__ == '__main__':
