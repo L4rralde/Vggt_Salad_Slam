94598fb8d9c2756a99be70834aa8d870331f9b9e
l4rralde <ealarralde@gmail.com>
2026-05-07 14:57:23 -0600

    Defining SL4Affine inv instead of relying on parent's implementation

--- src/align/homography/sl4.py
+++ src/align/homography/sl4.py
@@ -122,6 +122,18 @@ class SL4Affine(SL4):
         assert eps.shape == (12,)
         return eps
 
+    def inv(self) -> "SL4Affine":
+        A = self.mat[:3, :3]
+        t = self.mat[:3, 3]
+        A_inv = torch.linalg.inv(A)
+        t_inv = -A_inv @ t
+        mat_inv = torch.eye(
+            4,
+            dtype=self.mat.dtype,
+            device=self.mat.device
+        )
+        return SL4Affine(mat_inv)
+
     @classmethod
     def Exp(cls, x: torch.Tensor) -> "SL4Affine":
         assert x.shape == (12,)
