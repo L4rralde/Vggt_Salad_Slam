afbeb6d45590790bd76d3178e42f7358ccfad48a
l4rralde <ealarralde@gmail.com>
2026-07-28 14:21:51 -0600

    Adding abstract class for Pose Graph.

--- src/transforms/graphs.py
+++ src/transforms/graphs.py
@@ -1,5 +1,7 @@
 from typing import Hashable, Dict, Type, Tuple, List, Any
 from collections import OrderedDict
+from abc import ABC, abstractmethod
+from time import perf_counter
 
 import gtsam
 import numpy as np
@@ -39,9 +41,19 @@ def gtsam_sl4_to_matt_homography(gtsam_sl4: gtsam.SL4) -> matt.Homography:
     return matt.Homography(sl4_matrix)
 
 
+def matt_aff_to_gtsam(mat_transform: matt.Affine):
+    assert isinstance(mat_transform, matt.Affine)
+    return gtsam.Aff3(mat_transform._matrix)
 
-class Sim3Graph:
-    NDOF = 7
+
+def gtsam_to_matt_aff(gtsam_aff3) -> matt.Affine:
+    aff3_matrix = gtsam_aff3.matrix()
+    aff3_matrix /= aff3_matrix[3, 3]
+    return matt.Affine(aff3_matrix)
+
+
+class LieGraph(ABC):
+    NDOF = None
     def __init__(self):
         self.graph = gtsam.NonlinearFactorGraph()
         self.anchor_noise = gtsam.noiseModel.Diagonal.Sigmas(
@@ -54,43 +66,69 @@ class Sim3Graph:
         self._prior_estimations: Dict[int, matt.MatrixTransform] = OrderedDict()
         self._current_id = 0
 
+        self._matt_type: Type[matt.MatrixTransform]|None = None
+        self._gtsam_prior_factor_type = None
+        self._gtsam_between_factor_type = None
+    
     def add_anchor_prior(
         self,
-        id: Hashable,
-        est: matt.Sim3|None = None
+        node_id: Hashable,
+        est: matt.MatrixTransform|None = None
     ) -> None:
         if self._current_id != 0:
             raise RuntimeError("This must be the first node to add")
-        if id in self._ids_map:
+        if node_id in self._ids_map:
             raise RuntimeError("Id already in graph. This must be the first node to add")
         
         if est is None:
-            est = matt.Sim3()
+            est = self._matt_type()
         
-        self._ids_map[id] = self._current_id
+        type_check = isinstance(est, self._matt_type)
+        if not type_check:
+            try:
+                est = self._matt_type(est._matrix)
+            except:
+                raise ValueError(f"Estimation matrix is not compatible: {est}")
+        
+        self._ids_map[node_id] = self._current_id
         self._prior_estimations[self._current_id] = est
         self._current_id += 1
 
-        gtsam_sim3 = matt_sim3_to_gtsam(est)
+        gtsam_est = self.matt_to_gtsam(est)
  
         self.graph.add(
-            gtsam.PriorFactorSimilarity3(
-                0, gtsam_sim3, self.anchor_noise
+            self._gtsam_prior_factor_type(
+                0, gtsam_est, self.anchor_noise
             )
         )
+
+    @abstractmethod
+    def matt_to_gtsam(self, matt_matrix: matt.MatrixTransform) -> object:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def gtsam_to_matt(self, gtsam_lie_el: object) -> matt.MatrixTransform:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def result_at(self, result: object, index: int) -> object:
+        raise RuntimeError()
 
     def add_measurement(
         self,
         parent: Hashable,
         child: Hashable,
-        meas: matt.Sim3
+        meas: matt.MatrixTransform
     ) -> None:
         if not parent in self._ids_map:
             raise ValueError("Unknown parent")
         
-        type_check = isinstance(meas, matt.Sim3)
+        type_check = isinstance(meas, self._matt_type)
         if not type_check:
-            raise ValueError(f"Incorrect measurment type: {type(meas)}") 
+            try:
+                meas = self._matt_type(meas._matrix)
+            except:
+                raise ValueError(f"Incorrect measurment type: {type(meas)}, expected: {self._matt_type}") 
 
         child_exists = child in self._ids_map
         if not child_exists:
@@ -103,23 +141,23 @@ class Sim3Graph:
         if not child_exists:
             parent_est = self._prior_estimations[parent_id]
             child_est = parent_est @ meas
-            assert isinstance(child_est, matt.Sim3)
+            assert isinstance(child_est, self._matt_type)
             self._prior_estimations[child_id] = child_est
 
         self.graph.add(
-            gtsam.BetweenFactorSimilarity3(
+            self._gtsam_between_factor_type(
                 parent_id,
                 child_id,
-                matt_sim3_to_gtsam(meas),
+                self.matt_to_gtsam(meas),
                 noiseModel=self._meas_noise
             )
         )
-        
-    def optimize(self, verbose:bool=False) -> Tuple[Dict[Hashable, matt.Sim3]]:
+
+    def optimize(self, verbose:bool=False) -> Tuple[Dict[Hashable, matt.MatrixTransform]]:
         values = gtsam.Values()
         for i, est in self._prior_estimations.items():
-            assert isinstance(est, matt.Sim3)
-            values.insert(i, matt_sim3_to_gtsam(est))
+            assert isinstance(est, self._matt_type)
+            values.insert(i, self.matt_to_gtsam(est))
         
         params = gtsam.LevenbergMarquardtParams()
         optimizer = gtsam.LevenbergMarquardtOptimizer(self.graph, values, params)
@@ -132,8 +170,8 @@ class Sim3Graph:
 
         new_estimates = {}
         for i in self._prior_estimations.keys():
-            sim3_result = result.atSimilarity3(i)
-            new_estimates[reversed_ids_map[i]] = gtsam_to_matt_sim3(sim3_result)
+            gtsam_result = self.result_at(result, i)
+            new_estimates[reversed_ids_map[i]] = self.gtsam_to_matt(gtsam_result)
 
         if verbose:
             print(f"Previous error: {initial_error}")
@@ -143,8 +181,8 @@ class Sim3Graph:
 
         new_estimates = OrderedDict()
         for i in self._prior_estimations.keys():
-            sim3_result = result.atSimilarity3(i)
-            new_estimates[reversed_ids_map[i]] = gtsam_to_matt_sim3(sim3_result)
+            gtsam_result = self.result_at(result, i)
+            new_estimates[reversed_ids_map[i]] = self.gtsam_to_matt(gtsam_result)
         
         prev_estimates = OrderedDict({
             reversed_ids_map[i]: est
@@ -156,11 +194,11 @@ class Sim3Graph:
             OrderedDict(sorted(new_estimates.items()))
         )
 
-    def eval(self, estimations: Dict[hasattr, matt.Sim3]) -> Dict[str, Any]:
+    def eval(self, estimations: Dict[Hashable, matt.MatrixTransform]) -> Dict[str, Any]:
         values = gtsam.Values()
         for i, est in estimations.items():
-            assert isinstance(est, matt.Sim3)
-            values.insert(self._ids_map[i], matt_sim3_to_gtsam(est))
+            assert isinstance(est, self._matt_type)
+            values.insert(self._ids_map[i], self.matt_to_gtsam(est))
         
         error = self.graph.error(values)
         marginals = gtsam.Marginals(self.graph, values)
@@ -178,140 +216,80 @@ class Sim3Graph:
             'variances': OrderedDict(sorted(est_variances.items()))
         }
 
+    def update_estimation(self, new_estimations: Dict[Hashable, matt.MatrixTransform]) -> None:
+        new_inner_estimations = {
+            self._ids_map[k_id]: est
+            for k_id, est in new_estimations.items()
+        }
+        assert self._prior_estimations.keys() == new_inner_estimations.keys()
+        self._prior_estimations = new_inner_estimations
+    
+    def get_prior_estimations(self):
+        reversed_ids_map = {int_id: key for key, int_id in self._ids_map.items()}
 
- 
-class SL4Graph:
+        prev_estimates = OrderedDict({
+            reversed_ids_map[i]: est
+            for i, est in self._prior_estimations.items()
+        })
+
+        return OrderedDict(sorted(prev_estimates.items()))
+
+
+class Sim3Graph(LieGraph):
+    NDOF = 7
+    def __init__(self):
+        super().__init__()
+        self._matt_type: Type[matt.Sim3] = matt.Sim3
+        self._gtsam_prior_factor_type = gtsam.PriorFactorSimilarity3
+        self._gtsam_between_factor_type = gtsam.BetweenFactorSimilarity3
+
+    def matt_to_gtsam(self, sim3: matt.Sim3) -> gtsam.Similarity3:
+        return matt_sim3_to_gtsam(sim3)
+
+    def gtsam_to_matt(self, gtsam_sim3: gtsam.Similarity3) -> matt.Sim3:
+        return gtsam_to_matt_sim3(gtsam_sim3)
+
+    def result_at(self, result: object, index: int) -> object:
+        return result.atSimilarity3(index)
+
+
+class SL4Graph(LieGraph):
     NDOF = 15
     def __init__(self):
-        self.graph = gtsam.NonlinearFactorGraph()
-        self.anchor_noise = gtsam.noiseModel.Diagonal.Sigmas(
-            np.full(self.NDOF, 1e-6, dtype=float)
-        )
-        self._meas_noise = gtsam.noiseModel.Diagonal.Sigmas(
-            0.05*np.ones(self.NDOF, dtype=float)
-        )
-        self._ids_map: Dict[Hashable, int] = {}
-        self._prior_estimations: Dict[int, matt.MatrixTransform] = OrderedDict()
-        self._current_id = 0
+        super().__init__()
+        self._matt_type: Type[matt.Homography] = matt.Homography
+        self._gtsam_prior_factor_type = gtsam.PriorFactorSL4
+        self._gtsam_between_factor_type = gtsam.BetweenFactorSL4
 
-    def add_anchor_prior(
-        self,
-        id: Hashable,
-        est: matt.Sim3|None = None
-    ) -> None:
-        if self._current_id != 0:
-            raise RuntimeError("This must be the first node to add")
-        if id in self._ids_map:
-            raise RuntimeError("Id already in graph. This must be the first node to add")
-        
-        self._ids_map[id] = self._current_id
-        self._current_id += 1
-        
-        if est is None:
-            est = matt.Homography()
-        
-        self._prior_estimations[0] = est
-        gtsam_sl4 = matt_homography_to_gtsam_sl4(est)
- 
-        self.graph.add(
-            gtsam.PriorFactorSL4(
-                0, gtsam_sl4, self.anchor_noise
-            )
-        )
+    def matt_to_gtsam(self, homo: matt.Homography) -> gtsam.SL4:
+        return matt_homography_to_gtsam_sl4(homo)
 
-    def add_measurement(
-        self,
-        parent: Hashable,
-        child: Hashable,
-        meas: matt.Homography|matt.Affine
-    ) -> None:
-        if not parent in self._ids_map:
-            raise ValueError("Unknown parent")
-        
-        type_check = isinstance(meas, matt.MatrixTransform)
-        if not type_check:
-            raise ValueError(f"Incorrect measurment type: {type(meas)}") 
+    def gtsam_to_matt(self, gtsam_sl4: gtsam.SL4) -> matt.Homography:
+        return gtsam_sl4_to_matt_homography(gtsam_sl4)
 
-        child_exists = child in self._ids_map
-        if not child_exists:
-            self._ids_map[child] = self._current_id
-            self._current_id += 1
-        
-        parent_id = self._ids_map[parent]
-        child_id = self._ids_map[child]
-
-        if not child_exists:
-            parent_est = self._prior_estimations[parent_id]
-            child_est = parent_est @ meas
-            assert isinstance(child_est, matt.Homography)
-            self._prior_estimations[child_id] = child_est
-
-        self.graph.add(
-            gtsam.BetweenFactorSL4(
-                parent_id,
-                child_id,
-                matt_homography_to_gtsam_sl4(meas),
-                noiseModel=self._meas_noise
-            )
-        )
-        
-    def optimize(self, verbose:bool=False) -> Tuple[Dict[Hashable, matt.Sim3]]:
-        values = gtsam.Values()
-        for i, est in self._prior_estimations.items():
-            assert isinstance(est, matt.Homography)
-            values.insert(i, matt_homography_to_gtsam_sl4(est))
-        
-        params = gtsam.LevenbergMarquardtParams()
-        optimizer = gtsam.LevenbergMarquardtOptimizer(self.graph, values, params)
-
-        initial_error = self.graph.error(values)
-        result = optimizer.optimize()
-        final_error = self.graph.error(result)
-        if verbose:
-            print(f"Previous error: {initial_error}")
-            print(f"New error: {final_error}")
-
-        reversed_ids_map = {int_id: key for key, int_id in self._ids_map.items()}
-
-        new_estimates = OrderedDict()
-        for i in self._prior_estimations.keys():
-            sl4_result = result.atSL4(i)
-            new_estimates[reversed_ids_map[i]] = gtsam_sl4_to_matt_homography(sl4_result)
-        
-        prev_estimates = OrderedDict({
-            reversed_ids_map[i]: est
-            for i, est in self._prior_estimations.items()
-        })
-
-        return (
-            OrderedDict(sorted(prev_estimates.items())), 
-            OrderedDict(sorted(new_estimates.items()))
-        )
-
-    def eval(self, estimations: Dict[hasattr, matt.Homography]) -> Dict[str, Any]:
-        values = gtsam.Values()
-        for i, est in estimations.items():
-            assert isinstance(est, matt.Homography) or (est, matt.Affine)
-            values.insert(self._ids_map[i], matt_homography_to_gtsam_sl4(est))
-        
-        error = self.graph.error(values)
-        marginals = gtsam.Marginals(self.graph, values)
-        
-        reversed_ids_map = {int_id: key for key, int_id in self._ids_map.items()}
-        
-        est_variances = OrderedDict()
-        for int_key, ext_id in reversed_ids_map.items():
-            pose_covariance = marginals.marginalCovariance(int_key)
-            est_var = np.diag(pose_covariance)
-            est_variances[ext_id] = est_var
-        
-        return {
-            'error': error,
-            'variances': OrderedDict(sorted(est_variances.items()))
-        }
+    def result_at(self, result: object, index: int) -> object:
+        return result.atSL4(index)
 
 
-def average_transforms(transform_list: List[matt.Sim3]):
+class SL4Aff3Graph(LieGraph):
+    NDOF = 12
+    def __init__(self):
+        super().__init__()
+        self._matt_type:  Type[matt.MatrixTransform] = matt.Affine
+        self._gtsam_prior_factor_type = gtsam.PriorFactorAff3
+        self._gtsam_between_factor_type = gtsam.BetweenFactorAff3
+    
+    def matt_to_gtsam(self, matt_matrix: matt.MatrixTransform) -> object:
+        return matt_aff_to_gtsam(matt_matrix)
+
+    def gtsam_to_matt(self, gtsam_lie_el: object) -> matt.Affine:
+        return gtsam_to_matt_aff(gtsam_lie_el)
+    
+    def result_at(self, result: object, index: int) -> object:
+        return result.atAff3(index)
+
+
+def average_transforms(transform_list: List[matt.MatrixTransform]):
     if len(transform_list) == 1:
         return transform_list[0]
     
@@ -319,7 +297,9 @@ def average_transforms(transform_list: List[matt.Sim3]):
 
     if isinstance(pose_type(), matt.Sim3):
         graph = Sim3Graph()
-    elif isinstance(pose_type(), matt.Affine) or isinstance(pose_type, matt.Homography):
+    elif isinstance(pose_type(), matt.Affine):
+        graph = SL4Aff3Graph()
+    elif isinstance(pose_type, matt.Homography):
         graph = SL4Graph()
     else:
         raise ValueError(f"Transformation type {pose_type} not supported")
@@ -342,7 +322,9 @@ def pick_best_transform(transform_list: List[matt.Sim3]):
 
     if isinstance(pose_type(), matt.Sim3):
         graph_type = Sim3Graph
-    elif isinstance(pose_type(), matt.Affine) or isinstance(pose_type, matt.Homography):
+    elif isinstance(pose_type(), matt.Affine):
+        graph_type = SL4Aff3Graph
+    elif isinstance(pose_type, matt.Homography):
         graph_type = SL4Graph
     else:
         raise ValueError(f"Transformation type {pose_type} not supported")
@@ -362,6 +344,3 @@ def pick_best_transform(transform_list: List[matt.Sim3]):
             min_error = error
             best = transform
     return best
-    
-
-
